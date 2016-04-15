46a93abd7c2b28190bef2754369bd98f36bafce8
jestill2018 <jestill@mail.bradley.edu>
2016-04-15 02:23:47 -0500

    Test bullets spawning
    test bullets spawn, still need to move

---
+++ Beatdown/Assets/Scripts/Gameplay/Bullet_Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Movement : MonoBehaviour {
+		
+	/*void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.name == "Wall") {
+			Destroy (gameObject, 0);
+		}
+	}*/
+
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (Vector2.right * 10 * Time.deltaTime);
+		transform.Translate (Vector3.forward * 50 * Time.deltaTime);
+
+		if (transform.position.x > 10 || Collider.Equals(gameObject, GameObject.Find("Wall"))) {
+			Destroy (gameObject, 1);
+		}
+	}
+}
---
+++ Beatdown/Assets/Scripts/Gameplay/bullets.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullets : MonoBehaviour {
+
+	public Object bullet;
+	public GameObject player;
+
+	public Rigidbody2D rb2d;
+
+	public int bullPos;
+	public float accel;
+
+	Vector2 move;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		bullet = Instantiate (Resources.Load ("Bullet"), player.transform.position, Quaternion.identity);
+
+		//bullPos = player.transform.position;
+		move = new Vector2(accel, accel) * accel * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.P)) {
+			Instantiate (bullet, player.transform.position, Quaternion.identity);
+		}
+
+		//rb2d.AddForce (new Vector2 (Input.mousePosition.x, Input.mousePosition.y), ForceMode2D.Impulse);
+		rb2d.velocity = transform.TransformDirection (Vector3.forward * accel);
+	}
+}
