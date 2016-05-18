8cb572287318dff38d9e9f1c357074ecb21e66d9
zabbott <zabbott@mail.bradley.edu>
2016-05-18 13:54:08 -0500

    Made A few UI Scripts
    UI Scripts added

---
+++ Beatdown/Assets/Scripts/UIScripts/OnClickSceneTransition.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;  
+
+public class OnClickSceneTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayButton (); 
+	}
+
+	void PlayButton() { 
+		SceneManager.LoadScene ("gameplay");
+	}
+
+	void SettingsButton() { 
+
+
+	}
+
+	void QuitButton() { 
+
+
+	}
+
+	void CreditsButton() {
+
+
+	}
+
+	void RotateRight() { 
+
+
+	} 
+
+	void RotateLeft() { 
+
+
+	}
+
+
+
+
+}
