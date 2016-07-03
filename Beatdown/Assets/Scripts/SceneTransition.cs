ee227b48b851bfe70ad8c1f5e8d896b47aaa721c
zabbott <zabbott@mail.bradley.edu>
2016-07-02 22:55:28 -0400

    Some Of Country World has been built (VERY little)
    The designed level scenes have been started.

---
+++ Beatdown/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+
+public class SceneTransition : MonoBehaviour {
+
+	public string Scene; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) { 
+		if (other.gameObject.tag == "Player") {
+
+			SceneManager.LoadScene (Scene);
+		}
+	}
+}
