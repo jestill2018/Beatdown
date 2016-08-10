05985787d35591c0378b06e40b15a5c78cd97e00
zabbott <zabbott@mail.bradley.edu>
2016-08-10 12:04:47 -0500

    Punching now works amazingly
    Punch Script is now working perfect. Use a timer on the prefab to
    destroy it rather than having it destroy from the instantiation script.

---
+++ Beatdown/Assets/Scripts/PunchDestroyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchDestroyScript : MonoBehaviour {
+
+	private float PunchDestroyTimer = 20f; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PunchDestroyTimer--; 
+		print ("Punch Destroyer" + PunchDestroyTimer);
+
+		if (PunchDestroyTimer <= 0) { 
+			Destroy (gameObject);
+		
+			PunchDestroyTimer = PunchDestroyTimer;
+
+		} 
+	}
+}
